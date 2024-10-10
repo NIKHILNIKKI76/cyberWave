8480b787a1467c498270cfbd83735f4b2b48a10e
nikhil <nikhilchary919@gmail.com>
2024-10-10 21:30:53 +0530

    corrected local host problem in mongodb

--- src/components/certificateVerification.jsx
+++ src/components/certificateVerification.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Button from './Button'; // Assuming Button component is imported
 import Heading from './Heading';
 
+// Use the base URL from environment variables
+const baseUrl = process.env.REACT_APP_BASE_URL || 'https://cyberwave.info';
+
 function CertificateVerification() {
   const [certificateId, setCertificateId] = useState('');
 
@@ -9,7 +12,7 @@ function CertificateVerification() {
     event.preventDefault();
 
     // Build the verification link with the entered ID
-    const verificationLink = `http://localhost:5000/student?id=${certificateId}`;
+    const verificationLink = `${baseUrl}/api/student?id=${certificateId}`;
 
     // Open the EJS page in a new tab
     window.open(verificationLink, '_blank');
@@ -20,29 +23,28 @@ function CertificateVerification() {
 
   return (
     <section id="certificateVerification">
-          <div className="certificate-verification-container">
-      <Heading title="Certificate Verification" className="mt-4 opacity-90" /> {/* Adjusted margin top */}
-      <form onSubmit={handleSubmit} className="certificate-verification-form flex flex-col items-center">
-        <label htmlFor="certificateId" className="certificate-verification-label">
-          Enter Certificate ID:
-        </label>
-        <input
-          type="text"
-          id="certificateId"
-          value={certificateId}
-          onChange={(event) => setCertificateId(event.target.value)}
-          required
-          className="certificate-verification-input"
-        />
+      <div className="certificate-verification-container">
+        <Heading title="Certificate Verification" className="mt-4 opacity-90" /> {/* Adjusted margin top */}
+        <form onSubmit={handleSubmit} className="certificate-verification-form flex flex-col items-center">
+          <label htmlFor="certificateId" className="certificate-verification-label">
+            Enter Certificate ID:
+          </label>
+          <input
+            type="text"
+            id="certificateId"
+            value={certificateId}
+            onChange={(event) => setCertificateId(event.target.value)}
+            required
+            className="certificate-verification-input"
+          />
 
-        {/* Center the form elements using flexbox */}
-        <div className="certificate-verification-button-container mt-4">
-          <Button onClick={handleSubmit}>Verify</Button>
-        </div>
-      </form>
-    </div>
+          {/* Center the form elements using flexbox */}
+          <div className="certificate-verification-button-container mt-4">
+            <Button onClick={handleSubmit}>Verify</Button>
+          </div>
+        </form>
+      </div>
     </section>
-
   );
 }
 
